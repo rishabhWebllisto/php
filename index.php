d50d40464088468c911a551b9ff6537b1ed7028f
Rishabh k <rishabh.webllisto@gmail.com>
2019-07-23 17:40:52 +0530

    Update duplicate entry condition

--- index.php
+++ index.php
@@ -10,18 +10,30 @@ if (isset($_POST["import"])) {
         $file = fopen($fileName, "r");
         
         while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
-            $sqlInsert = "INSERT into users (userId,userName,password,firstName,lastName)
+           
+            $sqlInsertf = "SELECT * FROM users WHERE userName='$column[1]'";
+            
+            $result1 = mysqli_query($conn, $sqlInsertf);
+            $num_rows = mysqli_num_rows($result1);
+            if ($num_rows) {
+              $type = "error";
+                $message = "Problem in Importing CSV Data";
+            }else {
+                $sqlInsert = "INSERT into users (userId,userName,password,firstName,lastName)
                    values ('" . $column[0] . "','" . $column[1] . "','" . $column[2] . "','" . $column[3] . "','" . $column[4] . "')";
             
-            $result = mysqli_query($conn, $sqlInsert);
-            
-            if (! empty($result)) {
-                $type = "success";
-                $message = "CSV Data Imported into the Database";
-            } else {
-                $type = "error";
-                $message = "Problem in Importing CSV Data";
+                $result = mysqli_query($conn, $sqlInsert);
+                
+                if (! empty($result)) {
+                    $type = "success";
+                    $message = "CSV Data Imported into the Database";
+                } else {
+                    $type = "error";
+                    $message = "Problem in Importing CSV Data";
+                }
             }
+
+            
         }
     }
 }
